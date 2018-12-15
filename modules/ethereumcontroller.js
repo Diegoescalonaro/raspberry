6db673e577189ba6680800811037df79ed184d83
Diegoescalonaro <diegoescalonarodriguez@gmail.com>
2018-12-15 14:28:36 +0100

    Testing rinkeby network

--- modules/ethereumcontroller.js
+++ modules/ethereumcontroller.js
@@ -6,13 +6,14 @@ const ABI = require('./ABI').ABI;
 var web3 = new Web3(
     new Web3.providers.HttpProvider("http://localhost:8545")
 );
+web3.eth.defaultAccount = web3.eth.accounts[0]
 
 exports.web3 = web3
 //const eth = new Eth(new Eth.HttpProvider('https://kovan.infura.io/v3/901a0582278d4dd880c5e35a7f233cc2'));
 // endpoint: https://kovan.infura.io/v3/901a0582278d4dd880c5e35a7f233cc2
-// smart contract address: 0x257b195a3c5cd78a1238818ad9baa7ad9f24383a
-
-var contract = new web3.eth.Contract(ABI, '0x257b195a3c5cd78a1238818ad9baa7ad9f24383a')
+// smart contract address (KOVAN): 0x257b195a3c5cd78a1238818ad9baa7ad9f24383a
+// smart contract addres (RINKEBY): 0x96d1374d24d90faeb11413365d28482a26c63791
+var contract = new web3.eth.Contract(ABI, '0x96d1374d24d90faeb11413365d28482a26c63791')
 
 console.log("************ SMART CONTRACT *************************")
 // console.log(contract)
