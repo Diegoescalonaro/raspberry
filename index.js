d619f61c5bc83372c72c6b3f2e9c40c090e9507f
RaspberryPI <diegoescalonarodriguez@gmail.com>
2019-01-16 22:10:26 +0100

    index updated

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 /* Initial file */
 var config = require('./config')
+
+console.log("Nodo GETH light en ejecucion")
+
 var ethController = require('./modules/ethereumcontroller')
 var stockController = require('./modules/stockcontroller')
 
 exports.ethController = ethController
 exports.stockController = stockController
 
+console.log("Controlando el Stock de productos desde RaspberryPi")
 
 var wpi = require('node-wiring-pi');
 
@@ -15,8 +19,8 @@ wpi.pinMode(7, wpi.INPUT);
 setInterval(function () {
 
 if(wpi.digitalRead(7) == 0) {
-    console.log("Nueva solicitud de producto desde Raspberry PI")
-    ethController.solicitar("Probando solicitud desde boton", 1000000).then(console.log).catch(console.log)
+    console.log("* * * Nueva solicitud de producto desde RaspberryPi * * *")
+    ethController.solicitar("Producto [X] desde RaspberryPi", 1230000000000000).then(console.log).catch(console.log)
 }
 },1000)
 
